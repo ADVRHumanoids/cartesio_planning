d6ece55a7d677c170b68d63df8361b56b959f205
Arturo Laurenzi <arturo.laurenzi@iit.it>
2019-09-27 12:13:52 +0200

    Using planning scene monitor && simple object publisher

---
+++ src/planning_scene_publisher.cpp
@@ -0,0 +1,113 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Sachin Chitta, Michael Lautman */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
+// MoveIt!
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/GetStateValidity.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/ApplyPlanningScene.h>
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "planning_scene_ros_api_tutorial");
+
+    ros::NodeHandle node_handle;
+
+    ros::Publisher planning_scene_diff_publisher = node_handle.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+    ros::WallDuration sleep_t(0.5);
+    while (planning_scene_diff_publisher.getNumSubscribers() < 1)
+    {
+        sleep_t.sleep();
+    }
+
+    // Define the attached object message
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // We will use this message to add or
+    // subtract the object from the world
+    // and to attach the object to the robot
+    moveit_msgs::AttachedCollisionObject attached_object;
+    attached_object.link_name = "TCP_R";
+    /* The header must contain a valid TF frame*/
+    attached_object.object.header.frame_id = "TCP_R";
+    /* The id of the object */
+    attached_object.object.id = "box";
+
+    /* A default pose */
+    geometry_msgs::Pose pose;
+    pose.position.z = 0.12;
+    pose.orientation.w = 1.0;
+
+    /* Define a box to be attached */
+    shape_msgs::SolidPrimitive primitive;
+    primitive.type = primitive.BOX;
+    primitive.dimensions.resize(3);
+    primitive.dimensions[0] = 0.1;
+    primitive.dimensions[1] = 0.1;
+    primitive.dimensions[2] = 0.1;
+
+    attached_object.object.primitives.push_back(primitive);
+    attached_object.object.primitive_poses.push_back(pose);
+
+    // Note that attaching an object to the robot requires
+    // the corresponding operation to be specified as an ADD operation
+    attached_object.object.operation = attached_object.object.ADD;
+
+    // Since we are attaching the object to the robot hand to simulate picking up the object,
+    // we want the collision checker to ignore collisions between the object and the robot hand
+    attached_object.touch_links = std::vector<std::string>{ "RWrMot2", "RWrMot3", "r_wrist", "RSoftHandLink" };
+
+    // Add an object into the environment
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // Add the object into the environment by adding it to
+    // the set of collision objects in the "world" part of the
+    // planning scene. Note that we are using only the "object"
+    // field of the attached_object message here.
+    ROS_INFO("Adding the object into the world at the location of the hand.");
+    moveit_msgs::PlanningScene planning_scene;
+    planning_scene.world.collision_objects.push_back(attached_object.object);
+    planning_scene.is_diff = true;
+    planning_scene_diff_publisher.publish(planning_scene);
+
+}
--- src/planning_scene_test.cpp
+++ src/planning_scene_test.cpp
@@ -1,6 +1,8 @@
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_scene/planning_scene.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+
 #include <sensor_msgs/JointState.h>
 
 int main(int argc, char ** argv)
@@ -8,43 +10,47 @@ int main(int argc, char ** argv)
     ros::init(argc, argv, "planning_scene_test");
     ros::NodeHandle nh;
 
+    // load robot model from params
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
-    planning_scene::PlanningScene planning_scene(kinematic_model);
 
-    robot_state::RobotState& current_state = planning_scene.getCurrentStateNonConst();
+    // create planning scene
+    auto planning_scene = std::make_shared<planning_scene::PlanningScene>(kinematic_model);
 
-    // function to check if a joint name is defined inside the current state
-    // (useful to skip virtual joints, moveit uses quaterion)
-    auto has_joint_name = [&current_state](std::string jname)
+    // planning scene monitor automatically updates planning scene from topics
+    planning_scene_monitor::PlanningSceneMonitor monitor(planning_scene, "robot_description");
+
+    // on timer event callback
+    auto on_timer_event =
+            [&planning_scene,
+            &monitor](const ros::TimerEvent& msg)
     {
-        auto it = std::find(current_state.getVariableNames().begin(),
-                            current_state.getVariableNames().end(),
-                            jname);
-
-        return it != current_state.getVariableNames().end();
-    };
-
-    // on joint state received callback
-    auto on_js_received = [&current_state, &planning_scene, &has_joint_name](const sensor_msgs::JointStateConstPtr& msg)
-    {
-        for(int i = 0; i < msg->name.size(); i++)
-        {
-            if(!has_joint_name(msg->name[i]))
-            {
-                continue;
-            }
-
-            current_state.setJointPositions(msg->name[i], {msg->position[i]});
-        }
 
         collision_detection::CollisionRequest collision_request;
         collision_detection::CollisionResult collision_result;
-        planning_scene.checkSelfCollision(collision_request, collision_result);
-        ROS_INFO_STREAM("Test 1: Current state is " << (collision_result.collision ? "in" : "not in") << " self collision");
+        monitor.lockSceneRead();
+        planning_scene->checkCollision(collision_request, collision_result);
+        monitor.unlockSceneRead();
+        ROS_INFO_STREAM("Test 1: Current state is " << (collision_result.collision ? "in" : "not in") << " collision");
     };
 
-    auto js_sub = nh.subscribe<sensor_msgs::JointState>("joint_states", 1, on_js_received);
+    // every 0.5 sec we check for collisions
+    auto timer = nh.createTimer(ros::Duration(0.5), on_timer_event);
+
+    // publish planning scene at 30 Hz (topic is ~/monitored_planning_scene)
+    monitor.setPlanningScenePublishingFrequency(30.);
+
+    // this subscribes to /planning_scene
+    monitor.startSceneMonitor();
+
+    // this is somehow different from the scene monitor... boh
+    monitor.startWorldGeometryMonitor();
+
+    // this subscribes to /joint_states
+    monitor.startStateMonitor();
+
+    // this starts monitored planning scene publisher
+    monitor.startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE);
 
     ros::spin();
 }
