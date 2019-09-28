cd0f8c4707bfbff8bddc76b0b663536b63cec389
Arturo Laurenzi <arturo.laurenzi@iit.it>
2019-09-28 19:01:16 +0200

    Manual robot state update (requires cartesio on branch curstom_rspub)

--- src/planning_scene_test.cpp
+++ src/planning_scene_test.cpp
@@ -14,6 +14,10 @@ int main(int argc, char ** argv)
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
 
+    // tf
+    tf::TransformListener tf_listener;
+    tf_listener.waitForTransform("world", "base_link", ros::Time::now(), ros::Duration(3.0));
+
     // create planning scene
     auto planning_scene = std::make_shared<planning_scene::PlanningScene>(kinematic_model);
 
@@ -21,7 +25,7 @@ int main(int argc, char ** argv)
     planning_scene_monitor::PlanningSceneMonitor monitor(planning_scene, "robot_description");
 
     // on timer event callback
-    auto on_timer_event =
+    auto on_check_collisions_event =
             [&planning_scene,
             &monitor](const ros::TimerEvent& msg)
     {
@@ -34,8 +38,42 @@ int main(int argc, char ** argv)
         ROS_INFO_STREAM("Test 1: Current state is " << (collision_result.collision ? "in" : "not in") << " collision");
     };
 
+    // on timer event callback
+    auto on_joint_state_received =
+            [&monitor,
+            &tf_listener](const sensor_msgs::JointStateConstPtr& msg)
+    {
+        monitor.lockSceneWrite();
+
+        for(int i = 6; i < msg->name.size(); i++)
+        {
+            monitor.getPlanningScene()->getCurrentStateNonConst().setJointPositions(msg->name[i], {msg->position[i]});
+        }
+
+        tf::StampedTransform tf;
+        tf_listener.lookupTransform("world", "base_link", ros::Time(0), tf);
+
+        std::vector<double> qv = {
+            tf.getOrigin().x(),
+            tf.getOrigin().y(),
+            tf.getOrigin().z(),
+            tf.getRotation().x(),
+            tf.getRotation().y(),
+            tf.getRotation().z(),
+            tf.getRotation().w()
+        };
+
+        monitor.getPlanningScene()->getCurrentStateNonConst().setJointPositions("reference", qv);
+        monitor.unlockSceneWrite();
+
+        monitor.triggerSceneUpdateEvent(planning_scene_monitor::PlanningSceneMonitor::UPDATE_STATE);
+    };
+
     // every 0.5 sec we check for collisions
-    auto timer = nh.createTimer(ros::Duration(0.5), on_timer_event);
+    auto collisions_timer = nh.createTimer(ros::Duration(0.5), on_check_collisions_event);
+
+    // every 0.01 sec we update robot state
+    auto js_sub = nh.subscribe<sensor_msgs::JointState>("joint_states", 10, on_joint_state_received);
 
     // publish planning scene at 30 Hz (topic is ~/monitored_planning_scene)
     monitor.setPlanningScenePublishingFrequency(30.);
@@ -47,7 +85,10 @@ int main(int argc, char ** argv)
     monitor.startWorldGeometryMonitor();
 
     // this subscribes to /joint_states
-    monitor.startStateMonitor();
+//    monitor.startStateMonitor(); // we do this manually in the above callback "on_joint_state_received"
+
+
+
 
     // this starts monitored planning scene publisher
     monitor.startPublishingPlanningScene(planning_scene_monitor::PlanningSceneMonitor::UPDATE_SCENE);
