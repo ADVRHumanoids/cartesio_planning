55bb6cf526a192a9359c7dbfe7b8a3e37d51f137
Luca Rossini <luca.rossini@iit.it>
2021-03-26 16:01:36 +0100

    added log file for NSPG stats

--- log/untitled.m
+++ log/untitled.m
@@ -4,6 +4,9 @@ good = sum(success == 1);
 bad = sum(success == 0);
 
 average = sum(time) / length(time);
+average_ik = sum(time_ik)/length(time_ik);
+average_stability = sum(stability)/length(stability);
+average_collisions = sum(collisions)/length(collisions);
 
 figure
 plot([1:length(time)], time)
@@ -17,3 +20,8 @@ legend('collision check', 'centroidal statics check', 'IK solve')
 
 disp(["success rate is: ", good/length(success)*100])
 disp(["average: ", average])
+disp(["average IK: ", sum(time_ik)/length(time_ik)])
+disp(["average stability: ", sum(stability)/length(stability)])
+disp(["average collisions: ", sum(collisions)/length(collisions)])
+disp(["averagne #calls: ", average/(average_ik + average_stability + average_collisions)])
+% disp(["total time: ", sum(time)])
