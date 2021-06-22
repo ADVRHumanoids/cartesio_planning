7d209ff79211a499fd76af138b5b0ab193c8f3f7
Luca Rossini <luca.rossini@iit.it>
2021-06-22 11:34:21 +0200

    added log files and modified .m file

--- log/untitled.m
+++ log/untitled.m
@@ -8,6 +8,9 @@ average_ik = sum(time_ik)/length(time_ik);
 average_stability = sum(stability)/length(stability);
 average_collisions = sum(collisions)/length(collisions);
 
+% fails_coll = sum(coll_fails)/sum(tot_calls);
+% fails_stab = sum(stab_fails)/sum(tot_calls);
+
 figure
 plot([1:length(time)], time)
 
@@ -23,5 +26,9 @@ disp(["average: ", average])
 disp(["average IK: ", sum(time_ik)/length(time_ik)])
 disp(["average stability: ", sum(stability)/length(stability)])
 disp(["average collisions: ", sum(collisions)/length(collisions)])
-disp(["averagne #calls: ", average/(average_ik + average_stability + average_collisions)])
+disp(["average #calls: ", average/(average_ik + average_stability + average_collisions)])
+% disp(["% collision fails: ", fails_coll])
+% disp(["% stability fails: ", fails_stab])
 % disp(["total time: ", sum(time)])
+
+
